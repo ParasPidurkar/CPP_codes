bb55e5bf87717f1e202132e1063910e45a361b17
Paras Pidurkar <45849288+ParasPidurkar@users.noreply.github.com>
2022-02-26 20:56:07 +0530

    Create char_range.cpp

---
+++ char_range.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout<<"Hello World";
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //char ch =97;
+    
+   // cout<<ch;
+    
+    for( char ch =0 ;ch<= 254;ch++){  //infinite loop
+        cout<<ch<<"\n";
+    }
+
+    return 0;
+}
